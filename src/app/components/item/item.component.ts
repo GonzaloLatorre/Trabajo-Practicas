627fb8c1b57a34e43b17e9058fe37b80205da18f
GonzaloLatorre <89478526+GonzaloLatorre@users.noreply.github.com>
2023-07-01 13:09:22 -0300

    Realice las operaciones para sumar los precios de los productos

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -10,6 +10,8 @@ export class ItemComponent implements OnInit {
 
   @Input() item: Item = new Item();
   @Output() deleteItem: EventEmitter<Item>= new EventEmitter();
+  @Output() toggleItem: EventEmitter<Item>= new EventEmitter();
+  
   
   constructor() {}
 
@@ -22,5 +24,6 @@ export class ItemComponent implements OnInit {
 
   onToggle(item: Item){
     item.completed = !item.completed;
+    this.toggleItem.emit(item);
   }
 }
